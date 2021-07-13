4e0c46876ea4dfa918ff86f7fa7ec4f7154221f8
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-13 15:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from typing import Optional
 from telethon import TelegramClient as tg
 from fastapi import FastAPI
+import os
 import uuid
-
 from pydantic import BaseModel
-
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
 
 
 class Token(BaseModel):
@@ -20,9 +21,21 @@ async def read_root():
     return {"Hello": "World"}
 
 @app.put("/addtoken/{token}")
-async def create_item(token):
+async def create_bot(token):
+  bot = tg(token, API_ID, API_HASH).start(bot_token=token)
+  async with bot:
+    await bot.send_message("legendx22", "hello")
   res = {
      "method": "post",
      "response": token
+  }
+  return res
+
+@app.post("/token")
+async def create_item(token):
+  data = (await request.form())._dict
+  res = {
+     "method": "post",
+     "response": data["token"]
   }
   return res
