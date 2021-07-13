5b08e1060e346182865a2f64a7aa5c7576ddcd97
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-13 13:37:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,11 @@ from telethon import TelegramClient as tg
 from fastapi import FastAPI
 
 app = FastAPI()
-bot = tg("UltraXOp", 1621727, "31350903c528876f79527398c09660ce")
+#bot = tg("UltraXOp", 1621727, "31350903c528876f79527398c09660ce")
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-	  if q == None:
-	    return {"chat": item_id, "message": "not found message"}
-	  await bot.start()
-	  await bot.send_message(item_id, q)
-	  return {"chat": item_id, "message": q}
 
 @app.post("/pro/")
 async def create_item(item=True):
