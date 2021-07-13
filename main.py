bf7dc8445ccb98ee9fcb15ad14b53b517ffdc7d6
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-13 16:05:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ async def read_root():
 @app.put("/addtoken/{token}")
 async def create_bot(token):
   bot = tg(token, API_ID, API_HASH).start(bot_token=token)
+  print (dir(bot))
   await bot.send_message("legendx22", "hello")
   res = {
      "method": "post",
