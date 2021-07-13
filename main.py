7a50e1126e89d7b4f02b851dfdb2e983d22b69cc
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-13 13:36:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
+from typing import Optional
+from telethon import TelegramClient as tg
 from fastapi import FastAPI
 
 app = FastAPI()
-
+bot = tg("UltraXOp", 1621727, "31350903c528876f79527398c09660ce")
 
 @app.get("/")
-def read_root():
-    return {"200": "Welcome To Heroku"}
+async def read_root():
+    return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int, q: Optional[str] = None):
+	  if q == None:
+	    return {"chat": item_id, "message": "not found message"}
+	  await bot.start()
+	  await bot.send_message(item_id, q)
+	  return {"chat": item_id, "message": q}
 
-
-@app.get("/fizz_buzz/{num}")
-def read_item(num: int):
-    # https: // ja.wikipedia.org / wiki / Fizz_Buzz
-    if not num % 15:
-        return {num: "Fizz Buzz"}
-    elif not num % 5 or not num % 3:
-        return {num: 'Fizz' if not num % 3 else 'Buzz'}
-    else:
-        return {num: 'Stay Silent'}
+@app.post("/pro/")
+async def create_item(item=True):
+  res = {
+    	"method": "post",
+    	"response": item
+  }
+  return res
